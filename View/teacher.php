826f0b08161719e44fc60c63e1e06459448d5fcd
Jeroen-editing <jdv.editing@gmail.com>
2019-10-30 15:14:26 +0100

    adding teacher view

--- View/teacher.php
+++ View/teacher.php
@@ -42,17 +42,18 @@ require '../Controller/HomepageController.php';
                     <th>First Name</th>
                     <th>Last Name</th>
                     <th>Email</th>
-
+                    <th>Class</th>
                 </tr>
             </thead>
             <tbody>
             <?php
-            $sqlStudent = 'SELECT * FROM BeCodeDUO.student ORDER BY classID';
-            foreach ($openConnection->query($sqlStudent) as $row): ?>
+            $sqlTeacher = 'SELECT * FROM BeCodeDUO.teacher ORDER BY classID';
+            foreach ($openConnection->query($sqlTeacher) as $row): ?>
             <tr>
                     <td><?php echo $row['first_name'] ?></td>
                     <td><?php echo $row['last_name'] ?></td>
-                    <td><?php echo  $row['email'] ?></td>
+                    <td><?php echo $row['email'] ?></td>
+                    <td><?php echo $row['class_name'] ?></td>
                     </tr>
             <?php endforeach; ?>
             </tbody>

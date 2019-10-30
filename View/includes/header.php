70cf955a87d6a3a5403b32ab19964f76dac09dce
Jeroen-editing <jdv.editing@gmail.com>
2019-10-30 16:24:35 +0100

    adding header navbar

--- View/includes/header.php
+++ View/includes/header.php
@@ -1,3 +1,24 @@
 <header>
-    <h1>Welcome to my site</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand ml-5 mr-5" href="#">Becode</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link ml-5 mr-5" href="#">Home </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link ml-5 mr-5" href="#">Students<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link ml-5 mr-5" href="#">Teachers</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link ml-5 mr-5" href="#">Class Groups</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </header>
